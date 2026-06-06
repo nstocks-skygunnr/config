fd4261489d9fa446fd2e944ff5c788b46f7f4e95
Cory <cory.fitzsimmons@skygunnr.com>
2026-06-06 14:05:39 -0400

    using clk of gyro now.  Some cleanup

--- configs/GUNNRH7V1/config.h
+++ configs/GUNNRH7V1/config.h
@@ -26,6 +26,7 @@
 #define BOARD_NAME           GUNNRH7V1
 #define MANUFACTURER_ID      SKYG
 
+// remove once timers + DMA are sorted out
 #define USE_TIMER_MAP_PRINT
 
 #define USE_ACC
@@ -33,17 +34,15 @@
 #define USE_GYRO
 #define USE_GYRO_SPI_ICM42688P
 #define USE_BARO
-//#define USE_BARO_DPS368
 #define USE_BARO_DPS310
+#define USE_BARO_DPS368
 #define USE_SDCARD
 #define USE_MAX7456
 
-/*
+
 #define USE_GYRO_CLKIN
 #define GYRO_1_CLKIN_PIN     PA6
-#define GYRO_2_CLKIN_PIN	PB0
-*/
-
+#define GYRO_2_CLKIN_PIN     PB0
 
 #define UART1_TX_PIN         PA9
 #define UART1_RX_PIN         PA10
@@ -91,11 +90,11 @@
 //#define ADC_EXTERNAL1_PIN    PC4    // Airspeed
 
 #define SDCARD_DETECT_PIN    NONE 
-#define SDIO_DEVICE 		SDIODEV_1
-#define SDIO_USE_4BIT 1
-#define DEFAULT_BLACKBOX_DEVICE             BLACKBOX_DEVICE_SDCARD
+#define SDIO_DEVICE          SDIODEV_1
+#define SDIO_USE_4BIT        1
+#define DEFAULT_BLACKBOX_DEVICE    BLACKBOX_DEVICE_SDCARD
 
-#define PINIO1_PIN           PE2    // J5 PIN6
+#define PINIO1_PIN           PE2	// J5 PIN6
 #define PINIO2_PIN           PE3    // J5 PIN7
 #define PINIO3_PIN           PD10    // Camera Select
 #define PINIO4_PIN           PD11    // V10P0 EN
@@ -143,6 +142,7 @@
 #define SERVO1_PIN           PE9
 #define SERVO2_PIN           PE11
 
+// others
 #define BEEPER_PIN           PB14
 #define LED_STRIP_PIN        PE5 
 
@@ -155,49 +155,49 @@
     TIMER_PIN_MAP( 5,  MOTOR6_PIN,       1, 5 ) \
     TIMER_PIN_MAP( 6,  MOTOR7_PIN,       1, 6 ) \
     TIMER_PIN_MAP( 7,  MOTOR8_PIN,       1, 7 ) \
-	TIMER_PIN_MAP( 8,  SERVO1_PIN,		 1, -1) \
-	TIMER_PIN_MAP( 9,  SERVO2_PIN,		 1, -1) \
-    TIMER_PIN_MAP( 10, LED_STRIP_PIN,	 1, 8 ) \
-	TIMER_PIN_MAP( 11, BEEPER_PIN,       2, -1)
+    TIMER_PIN_MAP( 8,  SERVO1_PIN,	 1, -1) \
+    TIMER_PIN_MAP( 9,  SERVO2_PIN,	 1, -1) \
+    TIMER_PIN_MAP( 10, LED_STRIP_PIN,	 1, 14) \
+    TIMER_PIN_MAP( 11, BEEPER_PIN,       2, -1)
 
-#define ADC1_DMA_OPT        				9
-//#define ADC3_DMA_OPT       					10
-#define TIMUP3_DMA_OPT     					11
-#define TIMUP4_DMA_OPT     					12
-//#define TIMUP5_DMA_OPT     					13
+#define ADC1_DMA_OPT		9
+//#define ADC3_DMA_OPT       	10
+#define TIMUP3_DMA_OPT     	11
+#define TIMUP4_DMA_OPT     	12
+//#define TIMUP5_DMA_OPT     	13
 
-#define MAG_I2C_INSTANCE                    I2CDEV_1
-#define BARO_I2C_INSTANCE                   I2CDEV_2
-//#define DEFAULT_BARO_I2C_ADDRESS            118
+#define MAG_I2C_INSTANCE		I2CDEV_1
+#define BARO_I2C_INSTANCE               I2CDEV_2
 
-#define DEFAULT_CURRENT_METER_SOURCE        CURRENT_METER_ADC
-#define DEFAULT_VOLTAGE_METER_SOURCE        VOLTAGE_METER_ADC
-#define DEFAULT_CURRENT_METER_SCALE         85
+#define DEFAULT_CURRENT_METER_SOURCE    CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE    VOLTAGE_METER_ADC
+//#define DEFAULT_CURRENT_METER_SCALE     85
 #define BEEPER_INVERTED
 
-//#define DEFAULT_GYRO_TO_USE				GYRO_CONFIG_USE_GYRO_1
-//#define DEFAULT_GYRO_TO_USE				GYRO_CONFIG_USE_GYRO_2
-#define DEFAULT_GYRO_TO_USE 				GYRO_CONFIG_USE_GYRO_BOTH
-#define GYRO_1_SPI_INSTANCE                 SPI1
-#define GYRO_1_ALIGN                        CW0_DEG
-#define GYRO_2_SPI_INSTANCE                 SPI4
-#define GYRO_2_ALIGN                        CW270_DEG
+//#define DEFAULT_GYRO_TO_USE		GYRO_CONFIG_USE_GYRO_1
+//#define DEFAULT_GYRO_TO_USE		GYRO_CONFIG_USE_GYRO_2
+#define DEFAULT_GYRO_TO_USE 		GYRO_CONFIG_USE_GYRO_BOTH
+#define GYRO_1_SPI_INSTANCE             SPI1
+#define GYRO_1_ALIGN                    CW0_DEG
+#define GYRO_2_SPI_INSTANCE             SPI4
+#define GYRO_2_ALIGN                    CW270_DEG
 
-#define MAX7456_SPI_INSTANCE                SPI2
+#define MAX7456_SPI_INSTANCE            SPI2
 
-#define PINIO1_CONFIG                       1
-#define PINIO1_BOX                          40
-#define BOX_USER1_NAME                      "USER A"
-#define PINIO2_CONFIG                       1
-#define PINIO2_BOX                          41
-#define BOX_USER2_NAME                      "USER B"
-#define PINIO3_CONFIG                       1
-#define PINIO3_BOX                          42
-#define BOX_USER3_NAME                      "CAM SEL"
-#define PINIO4_CONFIG                       1
-#define PINIO4_BOX                          43
-#define BOX_USER4_NAME                      "10V EN"
+#define PINIO1_CONFIG		1
+#define PINIO1_BOX              40
+#define BOX_USER1_NAME          "USER A"
+#define PINIO2_CONFIG           1
+#define PINIO2_BOX              41
+#define BOX_USER2_NAME          "USER B"
+#define PINIO3_CONFIG           1
+#define PINIO3_BOX              42
+#define BOX_USER3_NAME          "CAM SEL"
+#define PINIO4_CONFIG           1
+#define PINIO4_BOX              43
+#define BOX_USER4_NAME          "10V EN"
 
 // user config defaults to match our design
-#define DEFAULT_RX_FEATURE			FEATURE_RX_SERIAL
-#define SERIALRX_UART				SERIAL_PORT_USART6
+#define DEFAULT_RX_FEATURE	FEATURE_RX_SERIAL
+#define SERIALRX_UART		SERIAL_PORT_USART6
+
